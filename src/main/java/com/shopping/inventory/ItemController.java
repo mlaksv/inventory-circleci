27cae223a17eb7aa6dfb5d020040ec5c6f11c000
mlaksv <mlaksv100@gmail.com>
2023-02-17 21:46:48 -0500

    testing code pipeline

--- src/main/java/com/shopping/inventory/ItemController.java
+++ src/main/java/com/shopping/inventory/ItemController.java
@@ -21,11 +21,11 @@ public class ItemController {
         return tableService.getAllItems();
     }
     
-    @GetMapping("/items/{id}")
+    /*@GetMapping("/items/{id}")
     public Optional<Item> getOneItemByID(@PathVariable Long id){
         //System.out.println("id is : "+id);
         return tableService.getOneItem(id);
-    }
+    }*/
     @PutMapping("/items/{id}")
     public Item updateItem(@PathVariable("id") Long id, @RequestBody Item item) throws ItemNotFoundException{
         return tableService.updateItem(id, item);
